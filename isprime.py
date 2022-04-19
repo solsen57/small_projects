ae9ea0d02837e75b17c52cc827a7e7562e9271b0
Sophie <sophieolsen57@outlook.com>
2022-04-19 18:54:38 -0500

    added error handling

--- isprime.py
+++ isprime.py
@@ -16,34 +16,53 @@ def main():
 
     #list primes up to/including number
     def listPrime(num):
-        i = 4
-        primes = [2, 3]
+        i = 2
+        primes = []
         while i <= num:
             if isPrime(i) == 1:
                 primes.append(i)
                 i+=1
             else:
                 i+=1
-        print('There are {} prime numbers between 2 and {}'.format(len(primes),num))
+        print('There are {} prime numbers between 1 and {}'.format(len(primes),num))
         print(primes)
 
-    gn = int(input('What number do you want to look at? '))
-    print('')
-    gn_choice = int(input('Would you like to: \n[1] State if your number is prime \n[2] State list of prime numbers up to/including your number \n[3] State both if your number is a prime and a list of prime numbers \n\n'))
+    #only accept positive integers
+    while True:
+        gn = int(input('What number do you want to look at? '))
+        if gn <= 0:
+            print('Please input a positive integer. \n')
+            continue
+        elif gn == 1:
+            print('1 is not a prime number and there are no prime numbers before 1. \n')
+            continue
+        else:
+            break
+
     print('')
 
-    if gn_choice == 1:
-        if isPrime(gn) == 1:
-            print('{} is a prime number!'.format(gn))
+    while True:
+        gn_choice = int(input('Would you like to: \n[1] State if your number is prime \n[2] State list of prime numbers up to/including your number \n[3] State both if your number is a prime and a list of prime numbers \n\n'))
+        print('')
+
+        if gn_choice == 1:
+            if isPrime(gn) == 1:
+                print('{} is a prime number!'.format(gn))
+            else:
+                print('{} is not a prime number'.format(gn))
+            break
+        elif gn_choice == 2:
+            listPrime(gn)
+            break
+        elif gn_choice == 3:
+            if isPrime(gn) == 1:
+                print('{} is a prime number!'.format(gn))
+            else:
+                print('{} is not a prime number'.format(gn))
+            listPrime(gn)
+            break
         else:
-            print('{} is not a prime number'.format(gn))
-    elif gn_choice == 2:
-        listPrime(gn)
-    elif gn_choice == 3:
-        if isPrime(gn) == 1:
-            print('{} is a prime number!'.format(gn))
-        else:
-            print('{} is not a prime number'.format(gn))
-        listPrime(gn)
+            print('Invalid input. \n')
+            continue
 
 main()
