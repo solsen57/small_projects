f4d164c53c095c1c4fdc0ef9b6d441caea350554
Sophie <sophieolsen57@outlook.com>
2022-04-19 18:37:48 -0500

    added user input for function

--- isprime.py
+++ isprime.py
@@ -1,29 +1,49 @@
 #determine if a number is prime
-def isPrime(num):
-    i = 2
-    factors = []
-    while i < num:
-        if (num % i == 0):
-            factors.append(i)
-            i+=1
+def main():
+    def isPrime(num):
+        i = 2
+        factors = []
+        while i < num:
+            if (num % i == 0):
+                factors.append(i)
+                i+=1
+            else:
+                i+=1
+        if len(factors) != 0:
+            return 0
         else:
-            i+=1
-    if len(factors) != 0:
-        return 0
-    else:
-        return 1
+            return 1
 
-#list primes up to/including number
-def listPrime(num):
-    i = 4
-    primes = [2, 3]
-    while i <= num:
-        if isPrime(i) == 1:
-            primes.append(i)
-            i+=1
+    #list primes up to/including number
+    def listPrime(num):
+        i = 4
+        primes = [2, 3]
+        while i <= num:
+            if isPrime(i) == 1:
+                primes.append(i)
+                i+=1
+            else:
+                i+=1
+        print('There are {} prime numbers between 2 and {}'.format(len(primes),num))
+        print(primes)
+
+    gn = int(input('What number do you want to look at? '))
+    print('')
+    gn_choice = int(input('Would you like to: \n[1] State if your number is prime \n[2] State list of prime numbers up to/including your number \n[3] State both if your number is a prime and a list of prime numbers \n\n'))
+    print('')
+
+    if gn_choice == 1:
+        if isPrime(gn) == 1:
+            print('{} is a prime number!'.format(gn))
         else:
-            i+=1
-    print('There are {} prime numbers between 2 and {}'.format(len(primes),num))
-    print(primes)
+            print('{} is not a prime number'.format(gn))
+    elif gn_choice == 2:
+        listPrime(gn)
+    elif gn_choice == 3:
+        if isPrime(gn) == 1:
+            print('{} is a prime number!'.format(gn))
+        else:
+            print('{} is not a prime number'.format(gn))
+        listPrime(gn)
 
-listPrime(14)
+main()
