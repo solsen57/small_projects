e87745accd648047ce609bea872c22ba0de6707e
Sophie <sophieolsen57@outlook.com>
2022-04-30 14:19:56 -0500

    updated readme. error handling in matrix calculator

--- Matrix_Calculator/Main_Matrix_Calculator.py
+++ Matrix_Calculator/Main_Matrix_Calculator.py
@@ -1,8 +1,27 @@
 import matrix_builder as mb
 import numpy as np
 
-u_rw = int(input('How many rows are there? '))
-u_cl = int(input('How many columns are there? '))
+while True:
+    try:
+        u_rw = int(input('How many rows are there? '))
+        if u_rw <= 0:
+            print('Invalid input. Please input a positive integer.')
+        else:
+            break
+    except ValueError:
+        print('Invalid input. Please input a positive integer.')
+
+while True:
+    try:
+        u_cl = int(input('How many columns are there? '))
+        if u_cl <= 0:
+            print('Invalid input. Please input a positive integer.')
+        else:
+            break
+    except ValueError:
+        print('Invalid input. Please input a positive integer.')
+
+
 
 A = mb.bul_matrix(u_rw, u_cl)
 
--- README.md
+++ README.md
@@ -1,3 +1,8 @@
 # A Collection of Small Projects
 
-This is a collection of small projects I've coded to demonstrate my abilities and track my progression in learning Python
+This is a collection of small projects I've coded to demonstrate my abilities and track my progression in learning Python.
+
+## Files Included:
++ "isprime.py": This script asks for a positive integer then gives user the options of (1) calculating if the given number is prime, (2) giving a list of prime numbers up to/including the given number, or (3) both functions.
++ "Matrix_Calculator": The Main_Matrix_Calculator file prompts the user to input a matrix and allows that matrix to be multiplied by a number.
++ "RPS_Game": Play Rock, Paper, Scissors against the computer.
