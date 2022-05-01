8b0eddfc0e9f4f630cce4084683fe9d07be87246
Sophie <sophieolsen57@outlook.com>
2022-05-01 16:14:53 -0500

    added score keeping for rock, paper, scissors

--- RPS_Game/game.py
+++ RPS_Game/game.py
@@ -33,6 +33,8 @@ options = ['rock', 'paper', 'scissors']
 game_defs = {'r':'rock', 'p':'paper', 's':'scissors'}
 
 def draw():
+    player_score = 0
+    computer_score = 0
     uc = input('Rock, Paper, or Scissors? ').lower()[0]
     cc = random.choice(options)[0]
     rg_res = [uc,cc]
@@ -43,41 +45,53 @@ def draw():
 
     if uc == cc:
         print("It's a tie!")
-        return 0
+        return player_score, computer_score
     else:
         if ('r' in rg_res) and ('p' in rg_res):
             if rg_res.index('p') == 1:
                 print('Computer wins!')
-                return 1
+                computer_score += 1
+                return player_score, computer_score
             else:
                 print('You win!')
-                return 2
+                player_score += 1
+                return player_score, computer_score
         elif('r' in rg_res) and ('s' in rg_res):
             if rg_res.index('r') == 1:
                 print('Computer wins!')
-                return 1
+                computer_score += 1
+                return player_score, computer_score
             else:
                 print('You win!')
-                return 2
+                player_score += 1
+                return player_score, computer_score
         elif ('s' in rg_res) and ('p' in rg_res):
             if rg_res.index('s') == 1:
                 print('Computer wins!')
-                return 1
+                computer_score += 1
+                return player_score, computer_score
             else:
                 print('You win!')
-                return 2
+                player_score += 1
+                return player_score, computer_score
 
 
 if __name__ == '__main__':
-    draw()
+    user = 0
+    comp = 0
     while True:
-        answer = input('\nWould you like to play again? ').lower()
-        if answer.startswith('y'):
-            print('')
-            draw()
-        elif answer.startswith('n'):
-            print('Goodbye!')
-            exit()
-        else:
-            print('Invalid input. \n')
-            continue
+        p, c = draw()
+        user += p
+        comp += c
+        print(f'Your Score: {(user)}\nComputer Score: {(comp)}')
+        while True:
+            answer = input('\nWould you like to play again? ').lower()
+            if answer.startswith('y'):
+                print('')
+                break
+            elif answer.startswith('n'):
+                print('Goodbye!')
+                exit()
+            else:
+                print('Invalid input. \n')
+                continue
